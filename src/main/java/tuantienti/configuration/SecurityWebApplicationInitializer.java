c8f112873cd09ddcb9b9fb0dc3d4f14df7871962
anhtuan9 <42605422+anhtuan9@users.noreply.github.com>
2018-11-07 09:25:22 +0700

    first-commit

---
+++ src/main/java/tuantienti/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package tuantienti.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
